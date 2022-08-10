2c610d203357f11e258ff65828e5ffee4effd6db
Clay Coleman <claycoleman94@gamil.com>
2022-08-09 21:51:38 -0400

    SyntaxSugar

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SyntaxSugarExcerise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 4;
+            string response;
+
+            if (answer < 9)
+            {
+                response = answer + "is less than nine";
+            }
+            else
+            {
+                response = answer + "greater than or equal to nine";
+            }
+
+            // The Homework 
+
+            // var answer = 4;
+            // var responses = (answer < 9) ? $"{answer} is less than nine" : $:{answer} greater than or equal to nine";
+
+
+        }
+    }
+}
+
